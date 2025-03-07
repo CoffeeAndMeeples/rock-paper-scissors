bc73fd77fef47c1331065f715641cc226a3d03f5
Cody Jorgensen <137858034+CoffeeAndMeeples@users.noreply.github.com>
2025-03-06 18:41:58 -0600

    Implement playRound funtion

--- myscript.js
+++ myscript.js
@@ -27,6 +27,80 @@ function getHumanChoice() {
     // return the input
     return(humanChoice);
 }
+// create a function called playRound that takes 2 parameters of humanChoice and computerChoice
+// that compares them and returns the results of a winner or draw
+function playRound(humanChoice, computerChoice) {
+    //get the human choice and make lowercase
+   // let input = getHumanChoice();
+    //humanChoice = input.toLowerCase();
 
-let weapon = getHumanChoice();
-console.log(weapon);
+    // get the computer choice
+    //computerChoice = getComputerChoice();
+
+    //compare 2 choices to declare a winner or tie
+    let winner;
+    // handle ties
+    if(humanChoice === computerChoice) {
+        winner = "Tie!  You both chose " + humanChoice;
+    }
+    // handle computer choosing rock
+    else if (computerChoice === "rock") {
+        if (humanChoice === "paper"){
+            winner = "human";
+        }
+        else {
+            winner = "computer";
+        }
+
+    }
+
+    // computer choosing paper
+    else if (computerChoice === "paper") {
+        if (humanChoice === "scissors"){
+            winner = "human";
+        }
+        else {
+            winner = "computer";
+        }
+    }
+    // commputer choosing scissors
+    else {
+        if (humanChoice === "rock"){
+            winner = "human";
+        }
+        else {
+            winner = "computer";
+        }
+    }
+
+    //console.log the winner
+    if (winner === "Tie") {
+        console.log("Tie!");
+    }
+    else if(winner === "human") {
+        console.log("You Win! You chose " + humanChoice + " and the Computer chose " + computerChoice + "!");
+    }
+    else {
+        console.log("You Lose! You chose " + humanChoice + " and the Computer chose " + computerChoice + "!")
+    }
+
+    // increment the winner's score
+    if(winner === "human") {
+        humanScore++;
+    }
+    if(winner === "computer") {
+        computerScore++;
+    }
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log("Human Score: " + humanScore);
+console.log("Computer Score: " + computerScore);
+
